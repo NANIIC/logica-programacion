3e1dcce4d6dcea2b2d09159e8e5527ee89c74d04
anahi <luna.calderon@mi.unc.edu.ar>
2024-07-03 00:58:49 -0300

    comparacion con el operador ternario

--- uno/app.js
+++ uno/app.js
@@ -3,6 +3,7 @@ let numeroSecreto = 6;
 let numeroUsuario = 0;
 let intentos = 1;
 let palabaVeces = 'vez';
+let maximosIntentos = 3;
 
 while (numeroUsuario != numeroSecreto) {
 
@@ -14,19 +15,19 @@ while (numeroUsuario != numeroSecreto) {
     la comparación
     */
     if (numeroUsuario == numeroSecreto) {
-        alert (`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabaVeces}`);
+        alert (`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en  ${intentos} ${intentos == 1 ? 'vez' : 'veces' } `);
         //alert('El numero secreto es: '+numeroSecreto);
         //acerto la condicion
     } else {
         if (numeroUsuario > numeroSecreto){
             alert('El número secreto es menor');
         } else {
-            alert ('El úmero secreto es mayor');
+            alert ('El número secreto es mayor');
         }
         intentos = intentos + 1;
         palabaVeces = 'veces';
-        if (intentos > 3){
-            alert ('Llegaste al número máximo de intentos :C');
+        if (intentos > maximosIntentos){
+            alert (`Llegaste al número máximo de ${maximosIntentos} intentos :C`);
            //ponemos el break para que corte con el while
             break;
         }
