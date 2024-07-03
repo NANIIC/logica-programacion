9d0c8217c02fd5c226e4fb993ae32dfda0f61440
anahi <luna.calderon@mi.unc.edu.ar>
2024-07-03 11:33:50 -0300

    Desafio1

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -3,8 +3,8 @@
 let titulo = document.querySelector('h1');
 let parrafo = document.querySelector('p');
 //le insertamos el titulo
-titulo.innerHTML ='Juego del número secreto';
-parrafo.innerHTML = 'Indica un número del 1 al 10';
+titulo.innerHTML ='Mira Caritoo';
+parrafo.innerHTML = 'estoy haciendo una pagina';
 
 //declaramos la funcion que pusimos para la parte de index donde esta lo de click que le pusimos
 //onclick="intentoDeUsuario();"  todo eso dentro de un buton
