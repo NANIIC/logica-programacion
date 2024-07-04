1fd5708fd8e93c6fc381afd78027f31598cca054
anahi <luna.calderon@mi.unc.edu.ar>
2024-07-04 12:25:08 -0300

    Automatizamos asiganción de texto

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,15 +1,17 @@
 
-//Seleccionamos (elemento)titulo para editar (se ve en index)
-let titulo = document.querySelector('h1');
-let parrafo = document.querySelector('p');
-//le insertamos el titulo
-titulo.innerHTML ='Mira Caritoo';
-parrafo.innerHTML = 'estoy haciendo una pagina';
 
 //declaramos la funcion que pusimos para la parte de index donde esta lo de click que le pusimos
 //onclick="intentoDeUsuario();"  todo eso dentro de un buton
+  
+//automatizamos la asignación de texto  
+function asignarTextoElento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML =texto;
 
+  }
   function intentoDeUsuario(){
-alert ('click desde el botón');
+     alert ('click desde el botón');
 
-}
+    }
+asignarTextoElento('h1','Juego del número secreto!');
+asignarTextoElento('p','Indica un  número del 1 al 10!');
