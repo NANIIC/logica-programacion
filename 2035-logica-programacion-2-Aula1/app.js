ffe75f3c979353d8a6e63928503a2b48fd48a602
anahi <luna.calderon@mi.unc.edu.ar>
2024-07-06 00:09:50 -0300

    Mensaje de número de intentos

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,6 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
-
-
+let intentos = 1; //lo iniciamos en 1 porque almenos una vez va a tener que haber intentado para acertar
+console.log(numeroSecreto);  // testeamos con este al inicio fuera de la funcion para verificar que diga vez en vés de veces cuando ponemos la respuesta a la primera
 function asignarTextoElento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML =texto;
@@ -8,16 +8,17 @@ return;
   }
   function verificarIntento(){
         let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-
-     
+      
+        console.log(intentos);
         if (numeroDeUsuario === numeroSecreto){
-          asignarTextoElento ('p', 'Acertaste en número!');
+          asignarTextoElento ('p', `Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
         }else {
           if (numeroDeUsuario > numeroSecreto){
             asignarTextoElento ('p','El número secreto es menor');
           }else{
             asignarTextoElento ('p','El número secreto es mayor');
           }
+          intentos++;
         }
         return;
     }
