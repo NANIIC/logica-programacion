f41459e0eeeac49e25ae71628480647aad21d45f
anahi <luna.calderon@mi.unc.edu.ar>
2024-07-07 20:24:36 -0300

    revolvientdo la Recursividad

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,5 +1,7 @@
 let numeroSecreto=0;
 let intentos = 0; 
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
 function asignarTextoElento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -21,21 +23,38 @@ return;
             asignarTextoElento ('p','El número secreto es mayor');
           }
           intentos++;
+          limpiarCaja();
         }
         return;
     }
     function limpiarCaja(){
-      let valorCaja = document.querySelector('#valorUsuario');  //usamos querySelector con el id poniendolee #
-      valorCaja.value = '';
+       document.querySelector('#valorUsuario').value ='';  //usamos querySelector con el id poniendolee #
+      
     }
   function generarNumeroSecreto(){
-    return Math.floor( Math.random()*10 )+1; //Generamos que el número secreto se cree sólo
+    let numeroGenerado = Math.floor( Math.random()* numeroMaximo )+1; //Generamos que el número secreto se cree sólo
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo){
+      asignarTextoElento ('p','Ya se sortearon todos los números posibles');
+    }else{
+    //Si el número generado está incluido en la lista
+    //includes lo que va a hacer es recorrer el arreglo y verficiar si algo ya existe y devuelve booleano
+      if (listaNumerosSorteados.includes(numeroGenerado)){
+        return generarNumeroSecreto();
 
+      } else{
+        //guardamos el numero para que no vuelva a salir
+        listaNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+      }
+    }
   }
   function condicionesIniciales(){
     asignarTextoElento
     asignarTextoElento('h1','Juego del número secreto!');
-    asignarTextoElento('p','Indica un  número del 1 al 10');
+    asignarTextoElento('p',`Indica un  número del 1 al ${numeroMaximo}`);
     numeroSecreto = generarNumeroSecreto();
     intentos = 1; //lo iniciamos en 1 porque almenos una vez va a tener que haber intentado para acertar
     console.log(numeroSecreto);  // testeamos con este al inicio fuera de la funcion para verificar que diga vez en vés de veces cuando ponemos la respuesta a la primera
